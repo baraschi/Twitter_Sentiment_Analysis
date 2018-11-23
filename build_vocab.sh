079ffabdde54c6c7449f633341986e50b29aa90c
Arnaud Boissaye <arnaud.boissaye@gmail.com>
2018-11-23 14:00:55 +0100

    Update of run.sh
    ./run.sh full
    Runneras le toute sur le full dataset. ./run.sh
    Runneras sur seulement 10% du data set.

--- build_vocab.sh
+++ build_vocab.sh
@@ -2,4 +2,10 @@
 
 # Note that this script uses GNU-style sed. On Mac OS, you are required to first
 #    brew install gnu-sed --with-default-names
-cat Data/twitter-datasets/train_pos.txt Data/twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > Data/vocab.txt
+if [ "$1" == "full" ]; then
+  echo on the full data set
+  cat Data/twitter-datasets/train_pos_full.txt Data/twitter-datasets/train_neg_full.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > Data/vocab.txt
+else
+  echo only on 10% of the data set
+  cat Data/twitter-datasets/train_pos.txt Data/twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > Data/vocab.txt
+fi
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build_vocab.sh
+./build_vocab.sh $1
 ./cut_vocab.sh
 python3 pickle_vocab.py
 python3 cooc.py
