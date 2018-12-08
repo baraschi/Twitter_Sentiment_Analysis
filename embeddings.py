75ba8eb7da81f5f7ea73476d40c5ebc2f0543203
LM. Garret <max.garret@wanadoo.fr>
2018-12-08 14:36:42 +0100

    First draw for predictions: 0.841965

--- embeddings.py
+++ embeddings.py
@@ -1,12 +1,15 @@
 import fasttext
 
-def fasttext_model(tweets, model = "skipgram", tweets_col = "clean_tweet_no_stop", label_col = "label"):
+def create_labeled_csv(tweets, tweets_col = "clean_tweet", label_col = "label"):
     # create column with correct label format for fasttext: '__label__0 '
-    tweets['label_prefixed'] = tweets['label'].apply(lambda s: '__label__' + str(s) + ' ')
+    tweets['label_prefixed'] = tweets[label_col].apply(lambda s: '__label__' + str(s) + ' ')
     
     #generate csv file
-    tweets.to_csv("data/fasttext_train.txt", columns = ['label_prefixed','clean_tweet_no_stop'], index=False)
+    tweets.to_csv("data/fasttext_train.txt", columns = ['label_prefixed',tweets_col], index=False)
     
+    
+def fasttext_model(tweets, model = "skipgram", tweets_col = "clean_tweet", label_col = "label"):
+    create_labeled_csv(tweets,tweets_col, label_col)
     # Skipgram model
     if model == "skipgram":
         model = fasttext.skipgram("data/labels_train.txt", 'model_skipgram')
