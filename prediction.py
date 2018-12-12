1f090a9fe16cf03da96a46cc5d19ad09934e7596
LM. Garret <max.garret@wanadoo.fr>
2018-12-12 14:14:54 +0100

    fixed tfidf
    still need to fix bow

--- prediction.py
+++ prediction.py
@@ -35,14 +35,14 @@ def classify_bow(train, test, tweets_col = CLEAN_TWEET, filename = "bow"):
     
     # bag-of-words feature matrix
     train_bow = bow_vectorizer.fit_transform(train[tweets_col])
-    test_bow = bow_vectorizer.fit_transform(test[tweets_col])
+    test_bow = bow_vectorizer.transform(test[tweets_col])
     
     # splitting data into training and validation set
     xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'], random_state=42, test_size=0.3)
     lreg_validation = LogisticRegression()
     lreg_validation.fit(xtrain_bow, ytrain) # training the model
     prediction_validation = lreg_validation.predict(xvalid_bow) # predicting on the validation set
-    accuracy = accuracy_score(yvalid, prediction_validation)
+    accuracy = accuracy_score(prediction_validation,yvalid)
     
     # regression using test set
     lreg_test = LogisticRegression()
@@ -53,26 +53,30 @@ def classify_bow(train, test, tweets_col = CLEAN_TWEET, filename = "bow"):
     
     return accuracy
     
-def classify_tfidf(train, test, tweets_col = CLEAN_TWEET, filename = "tfidf"):
-    tfidf_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
+def classify_tfidf(train, test, tweets_col = CLEAN_TWEET, filename = "tfidf", max_df = 0.90, min_df = 0.2, max_features =1000):
+    tfidf_vectorizer = TfidfVectorizer(max_df=max_df, min_df=min_df, max_features=max_features, stop_words='english')
+
     # TF-IDF feature matrix
     train_tfidf = tfidf_vectorizer.fit_transform(train[tweets_col])
-    test_tfidf = tfidf_vectorizer.fit_transform(test[tweets_col])
+    test_tfidf = tfidf_vectorizer.transform(test[tweets_col])
    
     # splitting data into training and validation set
     xtrain_tfidf, xvalid_tfidf, ytrain, yvalid = train_test_split(train_tfidf, train['label'], random_state=42, test_size=0.3)
 
     lreg_validation = LogisticRegression()
     lreg_validation.fit(xtrain_tfidf, ytrain)
-    prediction_validation = lreg_validation.predict(xvalid_tfidf) # predicting on the validation set
-    accuracy = accuracy_score(yvalid, prediction_validation)
+    #prediction_validation = lreg_validation.predict(xvalid_tfidf) # predicting on the validation set
+    prediction_validation = lreg_validation.predict_proba(xvalid_tfidf) # predicting on the validation set
+    prediction_validation_int = prediction_validation[:,1] >= 0.5 # if prediction is greater than or equal to 0.5 than 1 else 0
+    prediction_validation_int = prediction_validation_int.astype(np.int)
+    accuracy = accuracy_score(prediction_validation_int,yvalid)
 
     # regression using test set
-    lreg_test = LogisticRegression()
-    lreg_test.fit(train_tfidf, train['label']) # training the model
-    prediction_test = lreg_test.predict(test_tfidf)
+    prediction_test = lreg_validation.predict_proba(test_tfidf) # predicting on the test set
+    prediction_test_int = prediction_test[:,1] >= 0.5 # if prediction is greater than or equal to 0.5 than 1 else 0
+    prediction_test_int = prediction_test_int.astype(np.int)
     
-    submission_to_csv(format_submission(prediction_test.tolist()), filename + "_" + tweets_col)
+    submission_to_csv(format_submission(prediction_test_int.tolist()), filename + "_" + tweets_col)
     
     return accuracy
     
